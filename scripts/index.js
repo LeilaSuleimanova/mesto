19a6e3a83a14f4a23ae95800dfd8aa80510ee5c9
Leila Suleimanova <leila_suleymanova@mail.ru>
2023-03-10 01:32:33 +0300

    Second

--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,20 @@ const popupElement = document.querySelector('.popup')
 const popupCloseButtonElement = popupElement.querySelector('.popup__close')
 const popupOpenButtonElement = document.querySelector('.profile__edit-button')
 const formElement = document.querySelector('.popup__form')
-const nameInput = formElement.querySelector('.popup__profile_name')
-const jobInput = formElement.querySelector('.popup__profile_info')
+const nameInput = formElement.querySelector('.popup__profile_type_name')
+const jobInput = formElement.querySelector('.popup__profile_type_info')
 const profileName = document.querySelector('.profile__title')
 const profileJob = document.querySelector('.profile__subtitle')
 
 const openPopup = function () {
-  popupElement.classList.add('popup_is-opened');
+  popupElement.classList.add('popup_opened');
   console.log('Open popup clicked');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 };
 
 const closePopup = function () {
-  popupElement.classList.remove('popup_is-opened');
+  popupElement.classList.remove('popup_opened');
 };
 
 popupOpenButtonElement.addEventListener("click", openPopup);
